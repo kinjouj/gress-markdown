a7687cf808fe5e9a0fa6b33ec167cf7c257b0341
kinjouj <kinjouj@gmail.com>
2025-05-19 16:42:07 +0900

    typo

--- lib/gress/markdown.rb
+++ lib/gress/markdown.rb
@@ -5,18 +5,17 @@ require "psych"
 
 module Gress
   module Markdown
-      def self.parse(data)
-        raise RuntimeError if data.nil?
-        metadata = nil
-        body = nil
+    def self.parse(data)
+      raise RuntimeError if data.nil?
+      metadata = nil
+      body = nil
+      md = data.match(/\A(?<metadata>---\s*\n.*?\n?)^(---\s*$\n?)/m)
+      raise RuntimeError, "missing metadata" if md.nil?
+      metadata = Psych.safe_load(md[:metadata], permitted_classes: [Time, DateTime], symbolize_names: true)
+      raise RuntimeError, "invalid metadata" if metadata.class != Hash
+      body = md.post_match
 
-          md = data.match(/\A(?<metadata>---\s*\n.*?\n?)^(---\s*$\n?)/m)
-          raise RuntimeError, "missing metadata" if md.nil?
-          metadata = Psych.safe_load(md[:metadata], permitted_classes: [Time, DateTime], symbolize_names: true)
-          raise RuntimeError, "invalid metadata" if metadata.class != Hash
-          body = md.post_match
-
-        return [ metadata, body ]
-      end
+      return [ metadata, body ]
+    end
   end
 end
