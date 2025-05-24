8ba5651dd1b6ac3ae39dbca0d57fcb09380cab11
kinjouj <kinjouj@gmail.com>
2025-05-25 04:49:21 +0900

    update

--- lib/gress/markdown.rb
+++ lib/gress/markdown.rb
@@ -1,21 +1,20 @@
-# frozen_string_literal: true
-
-require_relative "markdown/version"
 require "psych"
 
 module Gress
-  module Markdown
-    def self.parse(data, &block)
-      raise RuntimeError if data.nil?
+  class Markdown
+    def self.parse(data)
       metadata = nil
       body = nil
-      md = data.match(/\A(?<metadata>---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)/m)
-      raise RuntimeError, "missing metadata" if md.nil?
-      metadata = Psych.safe_load(md[:metadata], permitted_classes: [Time, DateTime], symbolize_names: true)
-      raise RuntimeError, "invalid metadata" if metadata.class != Hash
-      body = md.post_match
 
-      block.call([metadata, body])
+      match = data.match(/\A(?<header>---\s*\n.*?\n?)^(---\s*$\n?)/m)
+      metadata = Psych.safe_load(
+        match[:header],
+        permitted_classes: [Time, DateTime],
+        symbolize_names: true
+      )
+      body = match.post_match
+
+      return [ metadata, body ]
     end
   end
 end
