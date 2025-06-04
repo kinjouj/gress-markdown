4e3cceb5b8fb991b75843dec0dad345fb76ff532
kinjouj <kinjouj@gmail.com>
2025-06-05 02:54:55 +0900

    update

--- lib/gress/markdown.rb
+++ lib/gress/markdown.rb
@@ -4,9 +4,9 @@ module Gress
   class Markdown
     def self.parse(txt, options={ symbolize_names: true, permitted_classes: [Date, Time, Symbol]})
       match = txt.match(/\A(?<header>---\s*\n.*?\n?)^(---\s*$\n?)/m)
-      metadata = Psych.safe_load(match[:header], **options)
+      header = Psych.safe_load(match[:header], **options)
       body = match.post_match
-      [ metadata, body ]
+      [ header, body ]
     end
   end
 end
